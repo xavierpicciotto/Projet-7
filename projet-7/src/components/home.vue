a187b9fa0ab87e5d4ea0a4207f039933dc61d87e
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-05 14:55:01 +0100

    Create home.vue

---
+++ projet-7/src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img src="https://img2.freepng.fr/20180619/qve/kisspng-house-logo-facebook-home-building-old-home-5b28b0c4ebc498.6871660215293933489657.jpg" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+    }
+</script>
+
+<style>
+
+</style>
