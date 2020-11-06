c734f559fadbe8908faf5a0d0f9e17fb2f6e687f
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-06 18:44:28 +0100

    Update App.vue

--- projet-7/src/App.vue
+++ projet-7/src/App.vue
@@ -1,19 +1,8 @@
 <template>
   <div id="app">
-    <Home/>
+    <router-view />
   </div>
 </template>
-
-<script>
-import Home from './views/home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home,
-  }
-}
-</script>
 
 <style lang="scss">
 body {
