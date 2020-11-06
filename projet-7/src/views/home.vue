362c6cd98345c25ed562ca5c1bf9d0da842ff353
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-06 18:44:35 +0100

    Update home.vue

--- projet-7/src/views/home.vue
+++ projet-7/src/views/home.vue
@@ -3,40 +3,67 @@
         <div class="logo-container">
             <img src="../assets/groupomania.png" alt="">
         </div>
-        <h1>Login</h1>
         <i class="fas fa-house-user"></i>
+        <h1> {{ option }} </h1>
         <div>
             <form>
+                <!--identifiant-->
                 <label for="name-user">Nom utilisateur</label>
-                <input id="userName" type="text" name="name-user" placeholder="identifiant">
+                <input v-model="userAccount.name" id="userName" type="text" name="name-user" placeholder="identifiant">
+                <!--mot de passe-->
                 <label for="password-user">Password</label>
-                <input id="password" type="password" placeholder="password">
-                <label for="confirm-password-user">Confirm password</label>
-                <input id="confirmPassword" type="password" placeholder="confirm your password">
-                <div class="agreement-box">
+                <input v-model="userAccount.password" id="password" type="password" placeholder="password">
+                <!--confirmation du mot de passe-->
+                <label  for="confirm-password-user">Confirm password</label>
+                <input v-model="userAccount.confirmPassword" v-if="userSignUp" id="confirmPassword" type="password" placeholder="confirm your password">
+                <!--acceptation conditions-->
+                <div v-if="userSignUp"  class="agreement-box">
                     <input id="agreement" type="checkbox">
                     <p>J'accepte les conditions</p>
                 </div>
+                <!--valider-->
                 <button id="validate" type="submit">Valider</button>
             </form>
-            <div class="sign-up-box">
+            <!--change entre login et sign up-->
+            <div @click="changeLoginOption()" class="sign-up-box">
                 <i class="fas fa-sign-in-alt"></i>
-                <p>Inscription</p>
+                <p> {{ switchOption }} </p>
             </div>
 
         </div>
+        <router-link to="/about">About</router-link>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Home',
-        props: {
-            url: String
-        },
         data: () => {
-
-        }
+            return {
+                option: "Login",
+                switchOption : "SignUp",
+                userSignUp: false,
+                userAccount: {
+                    name: "",
+                    password: "",
+                    confirmPassword: "",
+                },
+            }
+        },
+        methods:  {
+            changeLoginOption() {
+                if(this.userSignUp === true){
+                this.userSignUp = false    
+                this.option = "Login"
+                this.switchOption = "SignUp"
+                    return
+                }
+                this.userSignUp = true;
+                this.option = "SignUp"
+                this.switchOption = "Login"
+                
+            }
+        },
     }
 </script>
 
@@ -46,19 +73,29 @@
         height: 100%;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
 
     form {
         display: flex;
         flex-direction: column;
-        align-items: center;
-        justify-content: center;
     }
+
     .agreement-box {
         display: flex;
         flex-direction: row;
         align-items: center;
     }
+    .sign-up-box {
+        margin-top: 5%;
+        background-color: cyan;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
     .fa-house-user {
         font-size: 9em;
         margin: 5%;
