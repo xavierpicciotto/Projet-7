79fcbac27d2bc4c418cf23fa983f8767a6ddb812
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-09 22:44:19 +0100

    Update App.vue

--- projet-7/src/App.vue
+++ projet-7/src/App.vue
@@ -6,19 +6,8 @@
 
 <style lang="scss">
 body {
-  margin: 0;
-  padding: 1% 3% 1%;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  width: 100%;
-  height: 100%;
-  border: 3px solid blue;
-  margin: 0;
+background: linear-gradient(to right, #9fcbff, #001280c7);
+  
 }
 </style>
+ 
