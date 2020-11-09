b684aee6febbcfc07dc542949a6b5c52e882a16b
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-09 22:44:29 +0100

    Update About.vue

--- projet-7/src/views/About.vue
+++ projet-7/src/views/About.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <router-link to="/">Home</router-link>
+    <h1>About Groupomania</h1>
+    <HelloWorld>si</HelloWorld>    
+    <router-link to="/">
+    <h2>HOME</h2>
+    </router-link>
   </div>
 </template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+    color: white;
+    font-size: 2.5em;
+    margin-bottom: 20%;
+  }
+  h2{
+    text-align: center;
+    font-size: 5em;
+  }
+</style>
