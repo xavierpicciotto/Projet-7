0ea774de484004605ca9debc56a746363087cc57
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-13 20:46:31 +0100

    Create Login.vue

---
+++ projet-7/src/views/Login.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="col-md-12">
+    <div class="card card-container">
+      <img
+        id="profile-img"
+        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        class="profile-img-card"
+      />
+      <form name="form" @submit.prevent="handleLogin">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            v-model="user.username"
+            v-validate="'required'"
+            type="text"
+            class="form-control"
+            name="username"
+          />
+          <div
+            v-if="errors.has('username')"
+            class="alert alert-danger"
+            role="alert"
+          >Username is required!</div>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            v-model="user.password"
+            v-validate="'required'"
+            type="password"
+            class="form-control"
+            name="password"
+          />
+          <div
+            v-if="errors.has('password')"
+            class="alert alert-danger"
+            role="alert"
+          >Password is required!</div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary btn-block" :disabled="loading">
+            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+            <span>Login</span>
+          </button>
+        </div>
+        <div class="form-group">
+          <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+    import User from '../models/user';
+    export default {
+        name: 'Login',
+        data: () => {
+            return {
+                user: new User("", ""),
+                realClient: false,
+                loading: false,
+                message: ''
+            }
+        },
+        computed: {
+            loggedIn() {
+                return this.$store.state.auth.status.loggedIn;
+            }
+        },
+        created() {
+            if (this.loggedIn) {
+                this.$router.push('/profile');
+            }
+        },
+        methods: {
+            handleLogin() {
+                this.loading = true;
+                this.$validator.validateAll().then(isValid => {
+                    if (!isValid) {
+                        this.loading = false;
+                        return;
+                    }
+
+                    if (this.user.username && this.user.password) {
+                        this.$store.dispatch('auth/login', this.user).then(
+                            () => {
+                                this.$router.push('/profile');
+                            },
+                            error => {
+                                this.loading = false;
+                                this.message =
+                                    (error.response && error.response.data) ||
+                                    error.message ||
+                                    error.toString();
+                            }
+                        );
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    body {
+        height: 100%;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+    }
+
+    img {
+        width: 200px;
+        height: auto;
+        border-radius: 15px;
+    }
+
+    .form-signin {
+        width: 100%;
+        max-width: 500px;
+        padding: 15px;
+        margin: auto;
+        margin-top: 5%;
+
+        p {
+            color: white;
+            font-size: 1em;
+        }
+
+    }
+
+    hr {
+        margin: 5%;
+    }
+
+    .form-signin .checkbox {
+        margin-top: 10%;
+        font-size: 1.5em;
+    }
+
+    .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+    }
+
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+
+    a {
+        color: #fd2d01;
+        text-decoration: none;
+    }
+</style>>
