b4be0912492c002e76cd15e7b4105a1449a8cc15
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-14 10:24:22 +0100

    Create App.vue

---
+++ projet-7/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+body {
+background: linear-gradient(to right, #9fcbff, #001280c7);
+  
+}
+</style>
+ 
