c5c3d7f3bb36603a73844bf0f87a2d18a5286355
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-14 14:33:34 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+
 
 const app = express();
 
+/*const cors = require("cors");
 var corsOptions = {
   origin: "http://localhost:3000"
 };
+app.use(cors(corsOptions));*/
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 // parse la requete en JSON
 app.use(bodyParser.json());
