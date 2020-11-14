245a5dd56a7155478187f58f8fd4c3e173a98717
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-14 14:33:57 +0100

    Update main.js

--- projet-7/src/main.js
+++ projet-7/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import VeeValidate from 'vee-validate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate)
 
 new Vue({
   store,
