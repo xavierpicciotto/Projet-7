10a6250467fcd967678e90e4aa80ede601353c3a
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-17 22:29:46 +0100

    Update Register.vue

--- projet-7/src/views/Register.vue
+++ projet-7/src/views/Register.vue
@@ -1,34 +1,42 @@
 <template>
     <div class="home-page text-center">
-        <div class="logo-container">
-            <img src="../assets/groupomania.svg" alt="">
-        </div>
         <div>
-            <form class="form-signup">
-                <h1 class="h3 mb-3 font-weight-normal text-white">Please sign up</h1>
-                <!--User name-->
-                <input v-model="user.name" type="text" id="inputName" class="form-control" placeholder="Your user name"
-                    required autofocus>
-                <hr>
-                <!--Mot de passe-->
-                <input v-model="user.password" type="password" id="newPassword" class="form-control"
-                    placeholder="Password" required>
-                <input v-model="comfirPassword" type="password" id="comfirmPassword" class="form-control"
-                    placeholder="Comfirm password" required>
-                <!--Checkbox-->
-                <div class="checkbox mb-3">
-                    <label class="text-white">
-                        <input v-model="agreeRules" class="checkbox" type="checkbox" value="verification"> I accepte the
-                        rules.
-                    </label>
+            <h2>Please register</h2>
+            <form name="form" @submit.prevent="handleRegister">
+                <div v-if="!successful">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input v-model="user.username" v-validate="'required|min:3|max:20'" type="text"
+                            class="form-control" name="username" />
+                        <div v-if="submitted && errors.has('username')" class="alert-danger">
+                            {{errors.first('username')}}</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input v-model="user.email" v-validate="'required|email|max:50'" type="email"
+                            class="form-control" name="email" />
+                        <div v-if="submitted && errors.has('email')" class="alert-danger">{{errors.first('email')}}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input v-model="user.password" v-validate="'required|min:6|max:40'" type="password"
+                            class="form-control" name="password" />
+                        <div v-if="submitted && errors.has('password')" class="alert-danger">
+                            {{errors.first('password')}}</div>
+                    </div>
+                    <div class="checkbox mb-3">
+                        <input v-model="agreeClient" class="checkbox" type="checkbox" value="verification"> I accepte
+                        the rules.
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success btn-block">Sign Up</button>
+                    </div>
+                    <hr>
+                    <router-link to="/">
+                        <button class="btn btn-lg btn-primary btn-block text-center" type="button">Home</button>
+                    </router-link>
                 </div>
-                <!--Page pour l'inscription-->
-                <button class="btn btn-lg btn-success btn-block text-center" type="submit">Register me</button>
-                <hr>
-                <!--Retour a l'accueil-->
-                <router-link to="/">
-                    <button class="btn btn-lg btn-primary btn-block text-center" type="button">Return login</button>
-                </router-link>
             </form>
 
             <!--informations annexes-->
@@ -40,31 +48,53 @@
 </template>
 
 <script>
+    import User from '../models/user';
+
     export default {
         name: 'Register',
-        data: () => {
+        data() {
             return {
-                user: {
-                    name: "",
-                    password: "",
-
-                },
-                comfirPassword: "",
-                agreeRules: false,
+                user: new User('', '', ''),
+                submitted: false,
+                successful: false,
+                message: '',
+                agreeClient: false,
+            };
+        },
+        computed: {
+            loggedIn() {
+                return this.$store.state.auth.status.loggedIn;
+            }
+        },
+        mounted() {
+            if (this.loggedIn) {
+                this.$router.push('/profile');
             }
         },
         methods: {
-            login() {
-                let user = {
-                    ...this.user
-                }
-                if (this.realClient === true) {
-                    user = JSON.stringify(user)
-                    console.log(user)
-                }
+            handleRegister() {
+                this.message = '';
+                this.submitted = true;
+                this.$validator.validate().then(isValid => {
+                    if (isValid) {
+                        this.$store.dispatch('auth/register', this.user).then(
+                            data => {
+                                this.message = data.message;
+                                this.successful = true;
+                            },
+                            error => {
+                                this.message =
+                                    (error.response && error.response.data) ||
+                                    error.message ||
+                                    error.toString();
+                                this.successful = false;
+                            }
+                        );
+                    }
+                });
             }
-        },
-    }
+        }
+    };
 </script>
 
 <style scoped lang="scss">
@@ -79,59 +109,37 @@
         background-color: #f5f5f5;
     }
 
-    img {
-        width: 200px;
-        height: auto;
-        border-radius: 15px;
+    input {
+        font-size: 2.5em;
     }
 
-    .form-signup {
-        width: 100%;
-        max-width: 500px;
-        padding: 15px;
-        margin: auto;
-        margin-top: 5%;
-
-        p {
-            color: white;
-            font-size: 1em;
-        }
-
-        .checkbox {
-            font-weight: 400;
-        }
-
-        hr {
-            margin: 5%;
-        }
-
-        .form-control {
-            position: relative;
-            box-sizing: border-box;
-            height: auto;
-            padding: 10px;
-            font-size: 16px;
-        }
-
-        input [type="text"] {
-            margin-bottom: 25px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-        }
-
-        input[type="password"] {
-            margin-bottom: 35px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-        }
-
-        .checkbox {
-            font-size: 1.5em;
-        }
+    label {
+        color: white;
+        font-size: 1.5em;
     }
 
-    a {
-        color: #fd2d01;
-        text-decoration: none;
+    .checkbox {
+        font-weight: 400;
+        color: white;
+    }
+
+    hr {
+        margin: 5%;
+    }
+
+    input [type="text"] {
+        margin-bottom: 25px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    input[type="password"] {
+        margin-bottom: 35px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+
+    .checkbox {
+        font-size: 1.5em;
     }
 </style>>
