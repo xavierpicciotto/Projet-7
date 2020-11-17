3379cff1d128c25ef225b8202a6709610da6e1c1
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-17 22:28:43 +0100

    Update user.routes.js

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -1,8 +1,10 @@
-const { authJwt } = require("../middleware");
+const {
+  authJwt
+} = require("../middleware");
 const controller = require("../controllers/user.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -11,7 +13,7 @@ module.exports = function(app) {
   });
   //route pour récupérer tout les contenus
   app.get("/api/test/all", controller.allAccess);
-  
+
   //route pour récupérer user content
   app.get(
     "/api/test/user",
