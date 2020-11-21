eb16b5dc18331ead8bf05318ef9fde2455bcc697
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-11-21 10:25:56 +0100

    Update auth.module.js

--- projet-7/src/store/auth.module.js
+++ projet-7/src/store/auth.module.js
@@ -3,17 +3,43 @@ import AuthService from '../services/auth.service';
 const user = JSON.parse(localStorage.getItem('user'));
 
 //vérification
-console.log("TOKEN" + JSON.stringify(user))
+console.log("USER proffile:  " + JSON.stringify(user));
 
-const initialState = user
-  ? { status: { loggedIn: true }, user }
-  : { status: { loggedIn: false }, user: null };
+const initialState = user ?
+  {
+    status: {
+      loggedIn: true
+    },
+    user
+  } :
+  {
+    status: {
+      loggedIn: false
+    },
+    user: null
+  };
 
 export const auth = {
   namespaced: true,
   state: initialState,
   actions: {
-    login({ commit }, user) {
+
+    delete({
+      commit
+    }, user) {
+      console.log('auth module user = '+ JSON.stringify(user))
+      return AuthService.delete(user)
+      .then(x => {
+        console.log('auth module PROMISE object= ' + JSON.stringify(x));
+        commit('deleteSuccess', x);
+        return Promise.resolve(x)
+      })
+      .catch(err => console.log("ERROR auth module"+err))
+    },
+
+    login({
+      commit
+    }, user) {
       return AuthService.login(user).then(
         user => {
           commit('loginSuccess', user);
@@ -25,11 +51,17 @@ export const auth = {
         }
       );
     },
-    logout({ commit }) {
+
+    logout({
+      commit
+    }) {
       AuthService.logout();
       commit('logout');
     },
-    register({ commit }, user) {
+
+    register({
+      commit
+    }, user) {
       return AuthService.register(user).then(
         response => {
           commit('registerSuccess');
@@ -60,6 +92,10 @@ export const auth = {
     },
     registerFailure(state) {
       state.status.loggedIn = false;
+    },
+    deleteSuccess(state) {
+      state.status.loggedIn = false;
+      state.user = null;
     }
   }
 };
