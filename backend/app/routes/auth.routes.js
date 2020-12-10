40c2eaf96654e4ccab5c9c9f991e78b10567d319
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-12-10 15:46:27 +0100

    Update auth.routes.js

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
+const { authJwt } = require("../middleware");
 
 module.exports = function(app) {
   //envois les headers dans la reponse
@@ -25,8 +26,8 @@ module.exports = function(app) {
   app.post("/api/auth/signin", controller.signin);
 
   //route pour la supression.
-  app.delete("/api/auth/:id",controller.deleteUser);
+  app.delete("/api/auth/delete/:id",authJwt.verifyToken,controller.deleteUser);
 
   //route pour la modification.
-  app.put("/api/auth/modify/:id",controller.modifyUser)
+  app.put("/api/auth/modify/:id",authJwt.verifyToken,[verifySignUp.checkDuplicateUsernameOrEmail], controller.modifyUser)
 };
