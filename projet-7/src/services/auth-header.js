e8ce39c22dc2677eeef1fad8ef8678d18bfd18fc
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-12-10 15:46:34 +0100

    Update auth-header.js

--- projet-7/src/services/auth-header.js
+++ projet-7/src/services/auth-header.js
@@ -1,6 +1,6 @@
 export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
-  
+    console.log("AUTH HEADER" +JSON.stringify(user))
     if (user && user.accessToken) {
       return { 'x-access-token': user.accessToken };
     } else {
