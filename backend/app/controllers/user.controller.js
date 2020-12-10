52e35178615d63df2ed312153b36b311aa8001b4
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-12-10 15:46:24 +0100

    Update user.controller.js

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,12 +1,18 @@
 //renvoie l'acces aux contenus
 exports.userBoard = (req, res) => {
-  res.status(200).send("You have acces to user content.");
+  res.status(200).send({
+    message: "You have acces to user content."
+  });
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("You have acces to admin content.");
+  res.status(200).send({
+    message: "You have acces to admin content."
+  });
 };
 
 exports.moderatorBoard = (req, res) => {
-  res.status(200).send("You have acces to moderator content.");
+  res.status(200).send({
+    message: "You have acces to moderator content."
+  });
 };
