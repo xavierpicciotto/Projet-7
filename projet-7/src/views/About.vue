f7f95378fa4f04e87f5a3c573363afe8c97582a3
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-12-11 17:52:25 +0100

    Update About.vue

--- projet-7/src/views/About.vue
+++ projet-7/src/views/About.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="about">
-    <h1>About Groupomania</h1>    
-    <router-link to="/">
-    <h2>HOME</h2>
-    </router-link>
+    <h1>About Groupomania</h1>
+    <img src="../assets/GRP.png" alt="Siège social groupomania">
+    <p>Notre entreprise, spécialisée dans la grande distribution, est en pleine expansion. Nous avons actuellement
+      plus de 600 collaborateurs et avons beaucoup recruté depuis quelques années. Nous étions uniquement
+      300 il y a 3 ans.</p>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: "About",
-}
+  export default {
+    name: "About",
+  }
 </script>
 
 <style lang="scss" scoped>
+  .about {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 5%;
+    img {
+      height: auto;
+      width: 100%;
+    }
+  }
+
   h1 {
     text-align: center;
     color: white;
-    font-size: 2.5em;
-    margin-bottom: 20%;
+    font-size: 2.7vh;
+    margin-top: 5%;
+    margin-bottom: 10%;
   }
-  h2{
-    text-align: center;
-    font-size: 5em;
+
+  p {
+    font-size: 3vh;
+    color: white;
   }
+
 </style>
